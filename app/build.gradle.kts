a905931bbed45277bb04cfc97675ffd9e64647e0
Takayuki Cho <takayuki.cho@MacBook-Pro-8.local>
2024-02-18 18:34:28 +0900

    added tutorial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,12 @@ android {
 
 dependencies {
 
+    implementation("io.karte.android:core:2.+") //イベントトラッキング機能を提供します。
+    implementation("io.karte.android:inappmessaging:2.+") //アプリ内メッセージ機能を提供します。
+    implementation("io.karte.android:notifications:2.+") //プッシュ通知の受信および効果測定機能を提供します。
+    implementation("io.karte.android:variables:2.+") //設定値配信機能を提供します。
+    implementation("io.karte.android:visualtracking:2.+") //ビジュアルトラッキング機能を提供します。※ 別途Gradle Pluginが必要です。
+    implementation("io.karte.android:inbox:0.+") // Push通知の送信履歴を取得する機能を提供します（β版）。
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
